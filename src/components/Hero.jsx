34dd0e59d5874060d607590fcb53c547b50303f7
jedtap <doom.slaver2@gmail.com>
2023-12-26 21:06:04 +0800

    completed hero

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,11 +1,21 @@
 import React from 'react'
+import '../styles/Hero.css';
 
 const Hero = () => {
   return (<>
-    <h1>I help people and businesses deliver more value through comprehensive IT project management solutions.</h1>
-    <h2>Ready to elevate your projects and deliver exceptional value? Let's discuss your IT project management needs.</h2>
-    <h2>Start here.</h2>
-    <h3>Contact Jed</h3>
+    <div className='hero-wrapper'>
+      <h1 className='value-statement'>I help people and businesses <span className='emphasize'>deliver more value</span> through comprehensive IT project management solutions.</h1>
+      <div className='cta'>
+        <h2>Ready to elevate your projects and deliver exceptional value? Let's discuss your IT project management needs.</h2>
+        <div className='right-arrow'>➜</div>
+        <div className='contact-jed'>
+          <h2>Start here.</h2>
+          <a className="nav-link" href="https://forms.gle/3DhefWUMXfsuUnEk8" target="_blank" rel="noopener noreferrer">
+            <button type="button" class="btn btn-info">Contact Jed</button>
+          </a>
+        </div>
+      </div>
+    </div>
   </>)
 }
 
