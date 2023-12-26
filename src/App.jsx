1b7e7ecd7b46fb7a9b18ca3ea0a647722578edb2
jedtap <doom.slaver2@gmail.com>
2023-12-26 09:45:19 +0800

    template

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,17 @@
 import React from 'react'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+
+
+import Projects from './components/Projects'
 
 const App = () => {
-  return (
-    <div>App</div>
-  )
+  return (<>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/projects' element={<Projects />} />
+      </Routes>
+    </BrowserRouter>
+  </>)
 }
 
 export default App
