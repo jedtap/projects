105050b8379408d30c2a6700b69330c4a4db146e
jedtap <doom.slaver2@gmail.com>
2023-12-27 15:39:15 +0800

    updated blog

--- src/components/Blog.jsx
+++ src/components/Blog.jsx
@@ -1,9 +1,34 @@
 import React from 'react'
+import BlogData from '../helpers/BlogData'
+
+
+import '../styles/Blog.css'
 
 const Blog = () => {
-  return (
-    <div>Blog</div>
-  )
+  return (<>
+    <div className='alt-color'>
+      <br />
+      <br />
+      <h1 className='section-name-alt'>TOP STORIES</h1>
+      <br />
+      <a href="https://www.linkedin.com/in/jed-tapiador/recent-activity/all/" target="_blank" rel="noopener noreferrer" title="View latest LinkedIn posts" className="linkedin-link">Check out more stories!</a>
+      <br />
+      <div className='blog-wrapper'>
+        { BlogData.map((item, key) => 
+          <div className='blog-unit' key={key}>
+            <a href={item.link} className="blog-link" target="_blank" rel="noopener noreferrer" title="Check out the LinkedIn post!">
+              <img className='blog-img' src={item.img} alt={item.alt} />
+              <p className='blog-link'>Read more</p>
+            </a>
+          </div>
+        )}
+      </div>
+      <br />
+      <br />
+      <br />
+      <br />
+    </div>
+  </>)
 }
 
 export default Blog
---
+++ src/helpers/BlogData.jsx
@@ -0,0 +1,13 @@
+import Story1 from '../images/story1.jpg';
+import Story2 from '../images/story2.jpg';
+import Story3 from '../images/story3.jpg';
+import Story4 from '../images/story4.jpg';
+
+const BlogData = [
+  { key: 701, img: Story1, alt: "Jed's LinkedIn in collaboration with LinkedIn news asia.", link: "https://www.linkedin.com/posts/jed-tapiador_the-wrap-up-wednesday-december-6-2023-activity-7138124875818299392-V7o0?utm_source=share&utm_medium=member_desktop"},
+  { key: 702, img: Story2, alt: "Jed's LinkedIn post on project tradeoffs.", link: "https://www.linkedin.com/posts/jed-tapiador_projectmanagement-strategy-engagement-activity-7135493313087885312-4UwP?utm_source=share&utm_medium=member_desktop"},
+  { key: 703, img: Story3, alt: "Jed's LinkedIn post on a cyberattack to a shipping port.", link: "https://www.linkedin.com/posts/jed-tapiador_major-ports-cyber-attack-caused-by-citrix-activity-7132949754845544448-6dJc?utm_source=share&utm_medium=member_desktop"},
+  { key: 704, img: Story4, alt: "Jed's LinkedIn post on leading a project.", link: "https://www.linkedin.com/posts/jed-tapiador_inspiration-projectmanagement-entrepreneurship-activity-7139780833493581824-HbLR?utm_source=share&utm_medium=member_desktop"},
+];
+
+export default BlogData;
