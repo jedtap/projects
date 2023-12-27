5199cf0fbcc2a30860dd7afcd29d908880d8cb15
jedtap <doom.slaver2@gmail.com>
2023-12-27 10:48:14 +0800

    services section

--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -1,9 +1,31 @@
 import React from 'react'
+import ServicesData from '../helpers/ServicesData'
+
+import '../styles/Services.css'
 
 const Services = () => {
-  return (
-    <div>Services</div>
-  )
+  return (<>
+    <div className='alt-color'>
+      <br />
+      <br />
+      <h1 className='section-name-alt'>SERVICES</h1>
+      <br />
+        <div className='service-list'>
+          { ServicesData.map((item, key) => 
+            <div className='service-unit' key={key}>
+              <img className='service-img' src={item.img} alt={item.alt} />
+              <div className='service-text'>
+                <h2 className='service-title'>{item.title}</h2>
+                <p className='service-desc'>{item.desc}</p>
+              </div>
+            </div>
+          )}
+        </div>
+      <br />
+      <br />
+      <br />
+    </div>
+  </>)
 }
 
 export default Services
---
+++ src/helpers/ServicesData.jsx
@@ -0,0 +1,17 @@
+import Project from '../images/collab.jpg';
+import Templates from '../images/letter-heads.jpg';
+import Pmo from '../images/proj-mgmt.jpg';
+import Speaker from '../images/speaker.jpg';
+import Discuss from '../images/discuss.jpg';
+import Coaching from '../images/coaching.jpg';
+
+const ServicesData = [
+  { key: 601, title: 'Project Management', img: Project, alt: 'Photo of people collaborating on a laptop smiling. Photo by Priscilla Du Preez on Unsplash', desc: "Running projects can be like juggling a dozen balls at once. We're here to simplify it. Our project management services take care of the details, timelines, and coordination, turning your project worries into high-fives of success."},
+  { key: 602, title: 'Tailored Project Templates', img: Templates, alt: 'Stack of template letterheads. Photo by Daria Nepriakhina on Unsplash', desc: "Planning a project can be like trying to build a puzzle without the picture. Our tailored templates are like a roadmap, making project planning easy, straightforward, and tailored to fit your unique needs. No more starting from scratch - just plug and play."},
+  { key: 603, title: 'PMO Strategy and Procedures', img: Pmo, alt: 'Post its on board. Photo by Daria Nepriakhina on Unsplash', desc: "Ever wished for a playbook to make projects run like a well-oiled machine? Our PMO strategy is just that. We help set up smart procedures and governance, turning your organization into a project success powerhouse."},
+  { key: 604, title: 'Need a speaker?', img: Speaker, alt: 'Person speaking to an audiance. Photo by Kevin Gonzalez on Unsplash', desc: "Let's spice up your event with talks that demystify project management and Agile/Scrum in a way that's easy to grasp. Imagine engaging discussions that feel more like casual chats, where I share insights, real-world stories, and practical tips to make project management and agile concepts not only understandable but also downright interesting."},
+  { key: 605, title: 'Product Owner Services', img: Discuss, alt: 'People grouping together and discussing. Photo by Austin Distel on Unsplash', desc: "Think of it as your product's personal champion! As your dedicated Product Owner, I wear the hat of the customer advocate, making sure your product not only meets but exceeds expectations. From crafting features to collaborating with the development team, I'm here to help your product journey is smooth, exciting, and tailored to perfection."},
+  { key: 606, title: '1:1 Project Manager Career Coaching', img: Coaching, alt: 'Two people sitting down discussing. Photo by Medienstürmer on Unsplash', desc: "Feeling stuck in your project management career? Our 1:1 coaching sessions are like a personal compass, guiding you to new heights. Whether it's overcoming challenges, enhancing skills, or crafting a career roadmap, we're here to help you navigate the path to success."},
+];
+
+export default ServicesData;
